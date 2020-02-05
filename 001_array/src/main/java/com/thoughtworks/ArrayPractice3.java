3684d516f2eafbc9c1cbd9d2a98deffa9689383a
duyuqian <15738381799@163.com>
2020-02-05 16:16:44 +0800

    [duyuqian] feat: remove the elements with value 0 from the given array

--- 001_array/src/main/java/com/thoughtworks/ArrayPractice3.java
+++ 001_array/src/main/java/com/thoughtworks/ArrayPractice3.java
@@ -6,7 +6,20 @@ public class ArrayPractice3 {
      * 完成本方法实现功能: 将一个整型数组array中值0的元素去掉，并把剩下的元素组成新的数组
      */
     public static int[] filterZero(int[] array) {
-
-        return null;
+        int numberOfZero = 0;
+        for (int i=0;i<array.length;i++){
+            if(array[i]==0){
+                numberOfZero +=1;
+            }
+        }
+        int[] newArray = new int[array.length-numberOfZero];
+        int index = 0;
+        for (int i=0;i<array.length;i++){
+            if(array[i]!=0){
+                newArray[index] = array[i];
+                index++;
+            }
+        }
+        return newArray;
     }
 }
